9291d60b17b5b2240fa0bfe2b55c57136fabd8cc
thery <THERY340@gmail.com>
2025-05-08 23:43:12 +0200

    Formule Offre Page

--- src/app/pages/offres/offres.component.ts
+++ src/app/pages/offres/offres.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-offres',
-  standalone: true,
   templateUrl: './offres.component.html',
   styleUrls: ['./offres.component.css'],
 })
-export class OffresComponent {}
+export class OffresComponent implements OnInit {
+  isMobile = false;
+
+  ngOnInit() {
+    this.checkScreen();
+  }
+
+  @HostListener('window:resize')
+  checkScreen() {
+    this.isMobile = window.innerWidth <= 768;
+  }
+}
