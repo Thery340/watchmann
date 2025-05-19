f6fe9c9534b0fc5c2a8aff1d36ce20848e6d9e92
Thery340 <thery340@gmail.com>
2025-05-19 20:14:33 +0200

    1 Alignement boutton obtenir mon offre

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css'],
+  imports: [NgIf],
 })
 export class ContactComponent {
+  successMessage: string = '';
+  errorMessage: string = '';
 
+  onSubmit(event: Event) {
+    event.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_w5mjf8t', // Service ID EmailJS
+        'template_ltthxkc', // Template ID EmailJS (Contact Us)
+        event.target as HTMLFormElement,
+        '13kADPC_nLL6--0Co'
+      )
+      .then(
+        (result: EmailJSResponseStatus) => {
+          this.successMessage =
+            'Merci, votre message a bien été reçu. Notre équipe veille à vous répondre rapidement.';
+          this.errorMessage = '';
+          (event.target as HTMLFormElement).reset();
+        },
+        error => {
+          this.errorMessage = "Erreur d'envoi, veuillez réessayer.";
+          this.successMessage = '';
+        }
+      );
+  }
 }
